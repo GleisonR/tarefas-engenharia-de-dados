8580a22f44b958fac52dda29024eef32b77e78d6
GleisonR <gleisonro2012@gmail.com>
2023-05-29 16:26:53 -0300

    first commit

--- desafio_1.py
+++ desafio_1.py
@@ -3,13 +3,13 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.window import *
 
-#SUBINDO TABELAS DE DADOS PARA O BANCO.
+# Subindo tabelas de dados.
 
 %python
 df_desafio 1 = spark.read.format("csv").option("header","true").option("sep",",").option("encoding","UTF-8").load("/FileStore/tables/clientes.csv")
 display(desafio 1)
 
-#CRIANDO TABELA TEMPORÁRIA PARA COMANDOS EM SQL
+# Criando tabela temporária para comandos em SQL
 
 % python
 df_clientes . createOrReplaceTempView ( "clientes" )
@@ -20,7 +20,9 @@ df_resultado . createOrReplaceTempView ( "resultado" )
 %sql
 SELECT * FROM resultado
 
-#Quanto de rake foi gerado por cada Geração* de jogadores?
+# Quanto de rake foi gerado por cada Geração* de jogadores?
+
+# Geração Baby Boomers
 
 SELECT SUM(CAST(rake AS INT)) AS Rake_Baby_Boomers
 FROM clientes c
@@ -28,6 +30,7 @@ INNER JOIN resultado r
 ON c.id = r.clientes_id
 WHERE c.data_nascimento BETWEEN '1941-01-01' AND '1959-12-31'
 
+# Geração X
 
 SELECT SUM(CAST(rake AS INT)) AS Rake_GeracaoX
 FROM clientes c
@@ -35,6 +38,7 @@ INNER JOIN resultado r
 ON c.id = r.clientes_id
 WHERE c.data_nascimento BETWEEN '1960-01-01' AND '1979-12-31'
 
+# Geração Y
 
 SELECT SUM(CAST(rake AS INT)) AS Rake_GeracaoY
 FROM clientes c
@@ -42,6 +46,7 @@ INNER JOIN resultado r
 ON c.id = r.clientes_id
 WHERE c.data_nascimento BETWEEN '1980-01-01' AND '1995-12-31'
 
+# Geração Z
 
 SELECT SUM(CAST(rake AS INT)) AS Rake_GeracaoZ
 FROM clientes c
@@ -49,12 +54,14 @@ INNER JOIN resultado r
 ON c.id = r.clientes_id
 WHERE c.data_nascimento BETWEEN '1996-01-01' AND '2010-12-31'
 
+# Geração Alpha
 
-SELECT SUM(CAST(rake AS INT)) AS Rake_GeracaoZ
+SELECT data_nascimento, SUM(CAST(rake AS INT)) AS Rake_GeracaoAlpha
 FROM clientes c
 INNER JOIN resultado r
 ON c.id = r.clientes_id
-WHERE c.data_nascimento BETWEEN '1996-01-01' AND '2010-12-31'
+WHERE c.data_nascimento >= '2010-01-01'
+GROUP BY c.data_nascimento, r.rake
 
 # Os dados começam em 1942 e terminam em 2008. Com isso não tem dados Veteranos, geração formada por pessoas que nasceram entre 1925 e 1940 e Geração Alpha, engloba todos os nascidos a partir de 2010 até a presente data.
 
@@ -97,3 +104,5 @@ ON c.id = r.clientes_id
 WHERE c.sexo = 'f' 
 AND r.winning > 0
 GROUP BY c.sexo
+
+# Analizando os dados, o sexo com maior proporção de ganhadores é o Masculino
